ad86bcfcc695473c8115912b63bf7b0f05fd486f
SufalamUser001 <sunil.sufalam@gmail.com>
2026-04-17 13:25:49 +0530

    build

--- ios/App/App/public/chunk-K3SYRTHA.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./chunk-DGEX7UL3.js";import{f as e}from"./chunk-S5EYVFES.js";var o=class extends r{canOpenUrl(n){return e(this,null,function*(){return{value:!0}})}openUrl(n){return e(this,null,function*(){return window.open(n.url,"_blank"),{completed:!0}})}};export{o as AppLauncherWeb};
